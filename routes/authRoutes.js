4021d265bbfefe9661594190684409e395b86685
Vinibrito573 <viniciusmbrito@icloud.com>
2026-06-30 22:25:22 +0100

    Fixing routes folders structure and adding the authRoutes.js

---
+++ routes/authRoutes.js
@@ -0,0 +1,10 @@
+// Authentication routes for CertTrack
+// Mananing the login and logout
+// Vinicius Brito
+
+const express = require('express');
+const router  = express.Router();
+
+// IMPLEMENTATION ON GOING:
+
+module.exports = router;
