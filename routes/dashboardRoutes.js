1ff84aa5cb0e087f64219f597578bad27f1e02be
Vinibrito573 <viniciusmbrito@icloud.com>
2026-07-02 21:26:25 +0100

    Created Login and Dashboard page, and also  checking JWT authentication

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,9 +1,17 @@
-// Dashboard routes for CertTrack
+// In this file will be given the dashboard routes for CertTrack
 // Displays compliance KPIs and project overview
 // Vinicius Brito
 
 const express = require('express');
 const router  = express.Router();
+const { isAuthenticated, isAdmin } = require('../middleware/authMiddleware');
 
-// IMPLEMENTATION ON GOING:
+// Show dashboard page, which only admins can access
+router.get('/', isAuthenticated, isAdmin, (req, res) => {res.render('dashboard/index', { 
+    title: 'Dashboard',
+    user: req.user,
+    stats: { valid: 0, expiring: 0, expired: 0 } // placeholder for now
+  });
+});
+
 module.exports = router;
